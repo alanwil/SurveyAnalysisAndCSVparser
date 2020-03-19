c6065de36113eb0cd03ff354528db0a7571eb950
Alan <alan.wilmanski@gmail.com>
2020-03-19 23:32:09 +0100

    added AnswerSet class, possible problems with parsing empty answers

---
+++ src/main/java/Analyser.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Analyser {
+    private List<CSVObject> listToProcess;
+
+    public Analyser(List<CSVObject> listToProcess) {
+        this.listToProcess = listToProcess;
+    }
+
+
+}
---
+++ src/main/java/AnswersSet.java
@@ -0,0 +1,26 @@
+public class AnswersSet {
+    private String dateTime;
+    public static final String[] q1 = {"Ekologiczne, czyste i bezpieczne źródło energii","Elektrownie jądrowe","Katastrofa w Czarnobylu",
+                                        "Niebezpieczne odpady radioaktywne, problem z ich przechowywaniem","Radioaktywne skażenie środowiska",
+                                        "Globalna wojna nuklearna","Bombardowanie Hiroszimy i Nagasaki","Nie mam żadnych skojarzeń","Inne"};
+    public static final String[] q2 = {"Tak","Nie","Nie wiem"};
+    public static final String[] q3 = {"Jest bardzo często poruszany","Nie jest często poruszany","Nie jest w ogóle poruszany",
+                                        "Trudno powiedzieć","Nie korzystam ze środków masowego przekazu"};
+    public static final String[] q4 = {"Tak","Nie","Nie wiem"};
+    public static final String[] q5 = {"Tak","Nie","Nie wiem"};
+    public static final String[] q6 = {"Tak","Nie"};
+    public static final String[] q7 = {"Tak","Nie","Nie wiem"};
+    public static final String[] q8 = {"Tak","Nie","Nie wiem"};
+    public static final String[] q9 = {"Tak","Nie","Nie wiem"};
+    public static final String[] q10 = {"Tak","Nie","Nie wiem"};
+    public static final String[] q12 = {"nowe miejsca pracy","Polska powinna inwestować w energię jądrową","elektrownia jądrowa to najlepsze źródło energii",
+                                        "tego typu inwestycje to prestiż na arenie międzynarodowej","Inne:"};
+    public static final String[] q13 = {"boję się konsekwencji ewentualnych awarii lub katastrof (Czarnobyl, Fukushima itp)","boję się o zdrowie rodziny",
+                                        "uważam że takie elektrownie są niepotrzebne","widok elektrowni zepsułby krajobraz","Inne:"};
+    public static final String[] q14 = {"Tak","Nie","Nie wiem"};
+    public static final String[] gender = {"Mężczyzna","Kobieta"};
+    public static final String[] age = {"15-18","19-25","26-35","36-45","46-60","60+"};
+    public static final String[] education = {"wykształcenie podstawowe","wykształcenie gimnazjalne","wykształcenie zasadnicze zawodowe",
+                                            "wykształcenie średnie","wykształcenie wyższe","brak wykształcenia"};
+    public static final String[] citySize = {"Wieś","Miasto <20 000","Miasto 20 000 - 100 000","Miasto 100 000-500 000","Miasto 100 000-500 000"};
+}
