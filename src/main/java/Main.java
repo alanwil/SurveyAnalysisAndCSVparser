5835328711b363b57532d33a4b728bb576fa8cb8
Alan <alan.wilmanski@gmail.com>
2020-03-20 15:54:15 +0100

    added jcommons and jfreechart to dependencies and a pie chart example in main()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +18,17 @@ public class Main {
             list.add(parser.cvsParse(entry));
         }
 
+        DefaultPieDataset pieDataSet = new DefaultPieDataset();
+        pieDataSet.setValue("Value 1", 20);
+        pieDataSet.setValue("Value 2", 30);
+        pieDataSet.setValue("Value 3", 50);
 
+        JFreeChart chart = ChartFactory.createPieChart("Pie chart", pieDataSet,true,true,false);
+
+        try {
+            ChartUtils.saveChartAsJPEG(new File("src\\main\\resources\\piechart.jpeg"), chart, 500, 300);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
