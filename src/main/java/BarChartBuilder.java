216c96b2fb731a605bb03fcc31eeb4e4016cf9b9
Alan <alan.wilmanski@gmail.com>
2020-03-21 22:37:15 +0100

    somewhat working BarChartBuilder

--- src/main/java/BarChartBuilder.java
+++ src/main/java/BarChartBuilder.java
@@ -1,2 +1,40 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import java.io.File;
+import java.util.Map;
+
 public class BarChartBuilder {
+    private String question;
+    private Map<String, Integer> stats;
+
+    public BarChartBuilder(String question, Map<String, Integer> stats) {
+        this.question = question;
+        this.stats = stats;
+    }
+
+    public void createPieChart(String name){
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        for (Map.Entry<String, Integer> entry : stats.entrySet()) {
+            dataSet.setValue(entry.getValue(),"%", entry.getKey());
+        }
+
+        JFreeChart chart = ChartFactory.createBarChart(question, "", "%", dataSet,
+                                                    PlotOrientation.VERTICAL,false,true,false);
+//        PieSectionLabelGenerator label = new StandardPieSectionLabelGenerator("{2}", new DecimalFormat("0"), new DecimalFormat("0.0%"));
+//        PiePlot plot = (PiePlot) chart.getPlot();
+//        plot.setLabelGenerator(label);
+
+        StringBuilder path = new StringBuilder("src\\main\\resources\\");
+        path.append(name);
+        path.append(".jpeg");
+        try {
+            ChartUtils.saveChartAsJPEG(new File(path.toString()), chart, 800, 500);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,14 +12,18 @@ public class Main {
             list.add(parser.cvsParse(entry));
         }
         CSVObject questions = list.get(0);
+        AnswerCounter ac1 = new AnswerCounter();
+        ac1.q1(list);
+        BarChartBuilder q1chart = new BarChartBuilder(questions.getQ1()[0], ac1.getStats());
+        q1chart.createPieChart("q1chart");
 
-        Filter ageFilter = new Filter(list);
-        list = ageFilter.filterByAge(0);
-        AnswerCounter ac5 = new AnswerCounter();
-        ac5.q5(list);
+//        Filter ageFilter = new Filter(list);
+//        list = ageFilter.filterByAge(0);
+//        AnswerCounter ac5 = new AnswerCounter();
+//        ac5.q5(list);
 
-        PieChartBuilder q5chart = new PieChartBuilder(questions.getQ1()[0]+" w grupie "+AnswersSet.age[0]+" lat", ac5.getStats());
-        q5chart.createPieChart("q5byAge15-18");
+//        PieChartBuilder q5chart = new PieChartBuilder(questions.getQ1()[0]+" w grupie "+AnswersSet.age[0]+" lat", ac5.getStats());
+//        q5chart.createPieChart("q5byAge15-18");
 
 
 
